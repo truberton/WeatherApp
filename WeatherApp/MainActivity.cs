54710ff55d62187401cd830d8c16cdeeb41592a5
Truberton <Gert-Andry.Kaaramees@tptlive.ee>
2018-09-27 09:48:00 +0300

    button add

--- WeatherApp/MainActivity.cs
+++ WeatherApp/MainActivity.cs
@@ -14,6 +14,8 @@ namespace WeatherApp
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            //var button = FindViewById<Button>(Resource.Id.b)
         }
     }
 }
